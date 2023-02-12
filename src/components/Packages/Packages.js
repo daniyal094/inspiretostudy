a156ebb64398033815a395fd487ea094b2791e38
Daniyal Ahmed <daniyal@Daniyals-MacBook-Pro.local>
2023-02-12 07:25:29 -0800

    package error for vercel

--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
--- src/pages/course-list.js
+++ src/pages/course-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CourseListMain from '../components/CourseList/CourseListMain';
 import Footer from '../components/Layout/Footer/Footer';
 import Header from '../components/Layout/Header/Header';
-import Packages from '../components/Packages';
+import Packages from '../components/Packages/Packages';
 import HeaderThree from '../components/Layout/Header/HeaderStyleThree';
 
 class CourseList extends React.Component {
