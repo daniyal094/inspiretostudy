d1f32b147d7dc03ca8a0abb5987c4431474d7a10
Daniyal Ahmed <daniyal@Daniyals-MacBook-Pro.local>
2023-02-20 10:32:36 -0800

    Splash changes 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Layout/Header/Header";
 import Footer from "../components/Layout/Footer/Footer";
 import HomeMain from "../components/Home/HomeMain";
 import { useRouter } from "next/router";
-import SplashScreen from "../components/splashScreen";
+import SplashScreen from "../components/SplashScreen";
 import { useState } from "react";
 
 export default function index() {
